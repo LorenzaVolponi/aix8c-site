5f81b54e96c60f8008b26f94be0f187af7fe36a1
LorenzaVolponi <79290340+LorenzaVolponi@users.noreply.github.com>
2025-06-24 18:18:38 +0000

    fix: ensure proper default export of PortfolioSection

--- src/components/OptimizedIndex/LazyComponents.tsx
+++ src/components/OptimizedIndex/LazyComponents.tsx
@@ -1,6 +1,4 @@
-export { default as AboutSection } from "./AboutSection";
-export { default as SobreNosSection } from "./SobreNosSection";
-export { default as JornadaSection } from "./JornadaSection";
-export { default as PortfolioSection } from "./PortfolioSection";
-export { default as DepoimentosSection } from "./DepoimentosSection";
-export { default as ContatoSection } from "./ContatoSection";
+export { default as AboutSection } from './AboutSection';
+export { default as SobreNosSection } from './SobreNosSection';
+export { default as JornadaSection } from './JornadaSection';
+import PortfolioSection from './PortfolioSection'; export { PortfolioSection };
