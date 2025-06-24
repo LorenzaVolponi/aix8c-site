88373785438ca6573e67f87e768c13626744c678
LorenzaVolponi <79290340+LorenzaVolponi@users.noreply.github.com>
2025-06-24 11:21:08 -0300

    Add files via upload

--- src/components/OptimizedIndex/LazyComponents.tsx
+++
@@ -1,9 +0,0 @@
-// src/components/OptimizedIndex/LazyComponents.tsx
-import { lazy } from "react";
-
-export const AboutSection = lazy(() => import("./AboutSection"));
-export const SobreNosSection = lazy(() => import("./SobreNosSection"));
-export const JornadaSection = lazy(() => import("./JornadaSection"));
-export const PortfolioSection = lazy(() => import("./PortfolioSection"));
-export const DepoimentosSection = lazy(() => import("./DepoimentosSection"));
-export const ContatoSection = lazy(() => import("./ContatoSection"));
